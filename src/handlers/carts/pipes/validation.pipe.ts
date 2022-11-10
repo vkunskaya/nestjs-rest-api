2f2f07639fe92923fa673bd08ab5c5e77ffdfa4e
Volha Kunskaya <volha_kunskaya@epam.com>
2022-11-10 14:10:49 +0300

    feat: add git hooks

--- src/handlers/carts/pipes/validation.pipe.ts
+++ src/handlers/carts/pipes/validation.pipe.ts
@@ -3,11 +3,12 @@ import { CreateCartDtoValid } from '../dto/create-cart.dto';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
 
   transform(value: CreateCartDtoValid, metadata: ArgumentMetadata): CreateCartDtoValid {
     if (value.balance < 0) {
-      throw new BadRequestException('Balance cannot be less than 0')
+      throw new BadRequestException('Balance cannot be less than 0');
     }
     return value;
   }
